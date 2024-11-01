588f5b2e2665b1eaac8fece5728635648856f276
petrukhnov <konstantin.petrukhnov@gmail.com>
2024-11-01 20:01:46 +0000

    texture prep

--- core/src/main/kotlin/com/petrukhnov/konstantin/games/kotlearn/Main.kt
+++ core/src/main/kotlin/com/petrukhnov/konstantin/games/kotlearn/Main.kt
@@ -10,25 +10,38 @@ import com.badlogic.gdx.math.Rectangle
 
 class Main : ApplicationAdapter() {
 
-    val rect = Rectangle(100f, 100f, 100f, 100f)
+    //train
+    val train = Rectangle(100f, 500f, 100f, 100f)
+    var trainVerticalVelocity = 0f
+    val gravity = 0.1f
 
     override fun create() {
         super.create()
     }
 
-
     override fun render() {
 
-        Gdx.gl.glClearColor(33f, 0f, 0.2f, 1f)
+        Gdx.gl.glClearColor(0.5f, 1f, 0.5f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         val shapeRenderer: ShapeRenderer = ShapeRenderer()
-
-
         shapeRenderer.begin(ShapeType.Filled)
-        shapeRenderer.rect(100f,100f,100f,100f)
-        //rect.x += 1
-        //shapeRenderer.rect(rect.x, rect.y, rect.height, rect.width)
+        //shapeRenderer.rect(100f,100f,100f,100f)
+
+        //gravity
+        trainVerticalVelocity += gravity
+        train.y -= trainVerticalVelocity
+
+        if (train.y < 10) {
+            train.y = 10f
+            trainVerticalVelocity = 0f
+        }
+
+        if (trainVerticalVelocity == 0f && true) {
+            trainVerticalVelocity = -5f
+        }
+
+        shapeRenderer.rect(train.x, train.y, train.height, train.width)
         shapeRenderer.end()
     }
 }
