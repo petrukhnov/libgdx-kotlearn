9a29c6cd6c64718a3e5d3177939bbd8e5a595474
petrukhnov <konstantin.petrukhnov@gmail.com>
2024-12-30 22:43:30 +0000

    added color import

--- core/src/main/kotlin/com/petrukhnov/konstantin/games/kotlearn/Main.kt
+++ core/src/main/kotlin/com/petrukhnov/konstantin/games/kotlearn/Main.kt
@@ -2,6 +2,7 @@ package com.petrukhnov.konstantin.games.kotlearn
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
